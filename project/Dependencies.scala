6ffbd00dd5d2f24f2ff46f9a862ad2b8c65c8b2a
Scala Steward <scala_steward@virtuslab.com>
2026-07-24 18:29:59 +0000

    Update pekko-http, pekko-http-testkit to 1.4.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     val avro      = "1.12.0"
     val logback   = "1.5.18"
     val pekko     = "1.1.4"
-    val pekkoHttp = "1.2.0"
+    val pekkoHttp = "1.4.0"
     val scalaTest = "3.2.19"
   }
 
